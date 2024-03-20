b2c6133d1091bf7e534b99b8ecc71f1c7926b133
Lucas Balcerak <lucas.balcerak@gmail.com>
2024-03-20 17:13:07 +0100

    Config with custom post done

---
+++ mu-plugins/custom-post-type.php
@@ -0,0 +1,46 @@
+<?php
+
+function custom_post_type() {
+    register_post_type('About-us', array(
+        'show_in_rest' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'O nas',
+            'add_new' => 'Dodaj O nas',
+            'edit_item' => 'Edytuj O nas',
+            'all_items' => 'O nas',
+            'singular_name' => 'O nas'
+        ),
+        'menu_icon' => 'dashicons-megaphone'
+    ));
+
+    register_post_type('Products', array(
+        'show_in_rest' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Produkty',
+            'add_new' => 'Dodaj Produkt',
+            'edit_item' => 'Edytuj Produkt',
+            'all_items' => 'Produkty',
+            'singular_name' => 'Produkt'
+        ),
+        'menu_icon' => 'dashicons-list-view'
+    ));
+
+    register_post_type('Photo-gallery', array(
+        'show_in_rest' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Galleria',
+            'add_new' => 'Dodaj zdjęcie',
+            'edit_item' => 'Edytuj zdjęcie',
+            'all_items' => 'Zdjęcia',
+            'singular_name' => 'Zdjęcie'
+        ),
+        'menu_icon' => 'dashicons-format-gallery'
+    ));
+    
+}
+
+
+add_action('init', 'custom_post_type');
---
+++ readme.txt
@@ -0,0 +1,21 @@
+--POLSKA WERSJA--
+
+Przed użyciem theme
+1. Skopuj folder mu-plugins do .../app/public/wp-content
+2. Logo powino znajdować się w folderze image pod nazwą logo.png
+
+Ważne informacje:
+1. Posty O nas powienien zawierać tylko jeden post. Jeżeli jest ich więcej używany będzie tylko ostatni.
+2. Posty Produkty powinny zawierać tylko tytuł, reszta nie jest wyśiwetlana. 
+
+--ENGLISH VERSION--
+
+Things to do before using the theme:
+
+1. Copy the mu-plugins folder to .../app/public/wp-content
+2. Logo should be in folder image with name logo.png
+
+Important things:
+
+1. Custom post type About-us should have only one post, if it there are more, only the last one will be used
+2. Custom post type Products should have only a title. The content will not be displayed.
